75df81d18e8a5ba0d7e80e762545fce2b63577a7
Nazarii Baranovskyi <nazarii.baranovskyi@gmail.com>
2026-07-06 21:31:21 +0300

    chore: scaffold Vue 3 + Vuetify + Pinia + Vitest

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <h1 class="text-h5">Picturio</h1>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import vuetify from './plugins/vuetify'
+import App from './App.vue'
+
+createApp(App).use(createPinia()).use(vuetify).mount('#app')
